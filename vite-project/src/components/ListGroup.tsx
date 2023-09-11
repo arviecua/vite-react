b23a857bd43dc2c2b8710b5a3a0268050a080d51
arviecua <141235475+arviecua@users.noreply.github.com>
2023-09-12 07:54:48 +0800

    practicing dynamic values JSX

--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -1,14 +1,31 @@
-import { Fragment } from "react";
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  // const message = items.length === 0 ? <p>No Item Found</p> : null;
+
+  // Message Function
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>No Item Found</p> : null;
+  // };
+
+  // if (items.length === 0)
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No item found</p>
+  //     </>
+  //   );
+
   return (
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? <p>No Item Found</p> : null} */}
+      {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
