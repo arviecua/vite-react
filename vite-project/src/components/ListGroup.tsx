e84796535ec65aab574bfc13718adb2071603d0b
arviecua <141235475+arviecua@users.noreply.github.com>
2023-09-12 09:09:15 +0800

    hook practice and useState function

--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -1,30 +1,28 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
-
-  // const message = items.length === 0 ? <p>No Item Found</p> : null;
-
-  // Message Function
-  // const getMessage = () => {
-  //   return items.length === 0 ? <p>No Item Found</p> : null;
-  // };
-
-  // if (items.length === 0)
-  //   return (
-  //     <>
-  //       <h1>List</h1>
-  //       <p>No item found</p>
-  //     </>
-  //   );
+  // hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
-      {/* {items.length === 0 ? <p>No Item Found</p> : null} */}
+
       {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
@@ -32,3 +30,23 @@ function ListGroup() {
 }
 
 export default ListGroup;
+
+// import { MouseEvent } from "react";
+
+// event handler
+// const handleClick = (event: MouseEvent) => console.log(event);
+
+// const message = items.length === 0 ? <p>No Item Found</p> : null;
+
+// Message Function
+// const getMessage = () => {
+//   return items.length === 0 ? <p>No Item Found</p> : null;
+// };
+
+// if (items.length === 0)
+//   return (
+//     <>
+//       <h1>List</h1>
+//       <p>No item found</p>
+//     </>
+//   );
