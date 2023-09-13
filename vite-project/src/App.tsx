8d938650c725b8c6795b991945917a7471fffc84
arviecua <141235475+arviecua@users.noreply.github.com>
2023-09-13 09:35:47 +0800

    Started Props

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let colors = ["Red", "Violet", "Blue", "Green", "Yellow"];
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
+      <ListGroup items={colors} heading="Colors" />
     </div>
   );
 }
