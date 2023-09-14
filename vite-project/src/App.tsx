cc66473cafddc7f04fc8bbbc03e83913dc3d321b
arviecua <141235475+arviecua@users.noreply.github.com>
2023-09-14 19:30:06 +0800

    started practicing about passing children

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,9 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  let colors = ["Red", "Violet", "Blue", "Green", "Yellow"];
+  // let colors = ["Red", "Violet", "Blue", "Green", "Yellow"];
 
   const handleSelectItem = (items: string) => {
     console.log(items);
@@ -15,11 +16,13 @@ function App() {
         heading="Cities"
         onSelectItem={handleSelectItem}
       />
-      <ListGroup
+      {/* <ListGroup
         items={colors}
         heading="Colors"
         onSelectItem={handleSelectItem}
-      />
+      /> */}
+
+      <Alert />
     </div>
   );
 }
